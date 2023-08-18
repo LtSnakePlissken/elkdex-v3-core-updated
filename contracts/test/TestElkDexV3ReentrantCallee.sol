5e6d259f5983683dd53715fa59953a6cd39ecbdf
Ba'al <baal@elk.finance>
2023-08-19 00:30:53 +0200

    Renamed and tweaked for ElkDex deployment

---
+++ contracts/test/TestElkDexV3ReentrantCallee.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity =0.7.6;
+
+import '../libraries/TickMath.sol';
+
+import '../interfaces/callback/IElkDexV3SwapCallback.sol';
+
+import '../interfaces/IElkDexV3Pool.sol';
+
+contract TestElkDexV3ReentrantCallee is IElkDexV3SwapCallback {
+    string private constant expectedReason = 'LOK';
+
+    function swapToReenter(address pool) external {
+        IElkDexV3Pool(pool).swap(address(0), false, 1, TickMath.MAX_SQRT_RATIO - 1, new bytes(0));
+    }
+
+    function elkDexV3SwapCallback(
+        int256,
+        int256,
+        bytes calldata
+    ) external override {
+        // try to reenter swap
+        try IElkDexV3Pool(msg.sender).swap(address(0), false, 1, 0, new bytes(0)) {} catch Error(
+            string memory reason
+        ) {
+            require(keccak256(abi.encode(reason)) == keccak256(abi.encode(expectedReason)));
+        }
+
+        // try to reenter mint
+        try IElkDexV3Pool(msg.sender).mint(address(0), 0, 0, 0, new bytes(0)) {} catch Error(string memory reason) {
+            require(keccak256(abi.encode(reason)) == keccak256(abi.encode(expectedReason)));
+        }
+
+        // try to reenter collect
+        try IElkDexV3Pool(msg.sender).collect(address(0), 0, 0, 0, 0) {} catch Error(string memory reason) {
+            require(keccak256(abi.encode(reason)) == keccak256(abi.encode(expectedReason)));
+        }
+
+        // try to reenter burn
+        try IElkDexV3Pool(msg.sender).burn(0, 0, 0) {} catch Error(string memory reason) {
+            require(keccak256(abi.encode(reason)) == keccak256(abi.encode(expectedReason)));
+        }
+
+        // try to reenter flash
+        try IElkDexV3Pool(msg.sender).flash(address(0), 0, 0, new bytes(0)) {} catch Error(string memory reason) {
+            require(keccak256(abi.encode(reason)) == keccak256(abi.encode(expectedReason)));
+        }
+
+        // try to reenter collectProtocol
+        try IElkDexV3Pool(msg.sender).collectProtocol(address(0), 0, 0) {} catch Error(string memory reason) {
+            require(keccak256(abi.encode(reason)) == keccak256(abi.encode(expectedReason)));
+        }
+
+        require(false, 'Unable to reenter');
+    }
+}
